165b9e964c569ae790282f0291e0e91f850ea649
khamdullaevuz <elbek.khamdullaev@gmail.com>
2022-12-26 00:06:52 +0500

    adding some updates

--- views/home.view.php
+++ views/home.view.php
@@ -4,7 +4,7 @@ require 'layouts/header.layout.php';
 ?>
 <div class="container">
     <h1>Homepage</h1>
-    <p>This is site deployed with CI/CD</p>
+    <p>This is site deployed with CI/CD on github actions!</p>
 </div>
 <?php
 require 'layouts/footer.layout.php';
