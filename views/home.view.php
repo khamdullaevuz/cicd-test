0b6f10396a7ca1c1d0fc579cc4a907bb8f831df8
khamdullaevuz <elbek.khamdullaev@gmail.com>
2022-12-25 19:35:50 +0500

    configure for ci cd

--- views/home.view.php
+++ views/home.view.php
@@ -3,7 +3,8 @@ $title = "Homepage";
 require 'layouts/header.layout.php';
 ?>
 <div class="container">
-<h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <p>Deploy with ci/cd</p>
 </div>
 <?php
 require 'layouts/footer.layout.php';
