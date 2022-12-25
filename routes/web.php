89519a5fb3291ec700cb31350affb7a53f740f00
khamdullaevuz <elbek.khamdullaev@gmail.com>
2022-12-25 19:54:53 +0500

    remove products

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,3 @@ use Controllers\ProductController;
 
 Router::get('', HomeController::class);
 Router::get('about', [AboutController::class, 'about']);
-
-Router::get('product', [ProductController::class, 'index']);
-Router::get('product/show', [ProductController::class, 'show']);
-Router::get('product/edit', [ProductController::class, 'edit']);
-Router::get('product/add', [ProductController::class, 'add']);
-Router::delete('product/delete', [ProductController::class, 'delete']);
-Router::post('product/insert', [ProductController::class, 'insert']);
-Router::put('product/put', [ProductController::class, 'put']);
