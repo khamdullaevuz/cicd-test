f4be5f060ec3e93f9afae07d2368ac6068356132
khamdullaevuz <elbek.khamdullaev@gmail.com>
2022-12-26 00:43:27 +0500

    update home

--- views/home.view.php
+++ views/home.view.php
@@ -5,6 +5,7 @@ require 'layouts/header.layout.php';
 <div class="container">
     <h1>Hello world!</h1>
     <p>This is site deployed with CI/CD on github actions!</p>
+    <p><a href="https://github.com/khamdullaevuz/cicd-test">Repo</a></p>
 </div>
 <?php
 require 'layouts/footer.layout.php';
