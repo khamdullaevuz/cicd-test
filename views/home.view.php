4eeff943321b2bbf13d768d9bb322b30401e489d
Elbek Khamdullaev <elbek.khamdullaev@gmail.com>
2022-12-26 10:37:37 +0500

    customize home page

--- views/home.view.php
+++ views/home.view.php
@@ -5,7 +5,7 @@ require 'layouts/header.layout.php';
 <div class="container">
     <h1>Homepage</h1>
     <p>This is site deployed with CI/CD on github actions!</p>
-    <p><a class="text-decoration-none" href="https://github.com/khamdullaevuz/cicd-test">Repo</a></p>
+    <p><a class="text-decoration-none" href="https://github.com/khamdullaevuz/cicd-test" target="_blank">Repo</a></p>
 </div>
 <?php
 require 'layouts/footer.layout.php';
