d8c00e01933946cdadf7bd306cd3805209a8c380
Elbek Khamdullaev <elbek.khamdullaev@gmail.com>
2023-04-16 21:31:36 +0500

    Update home.view.php

--- views/home.view.php
+++ views/home.view.php
@@ -4,7 +4,7 @@ require 'layouts/header.layout.php';
 ?>
 <div class="container">
     <h1>Home</h1>
-    <p>This is site deployed with CI/CD on github actions!</p>
+    <p>This is site deployed with CI/CD on github actions</p>
     <p><a class="text-decoration-none" href="https://github.com/khamdullaevuz/cicd-test" target="_blank">Repo</a></p>
 </div>
 <?php
