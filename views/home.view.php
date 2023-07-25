441a6277e5ad642e8f7e5dc7bf5e752f4b2add75
Elbek Khamdullaev <elbek.khamdullaev@gmail.com>
2023-07-25 14:30:02 +0500

    Update home.view.php

--- views/home.view.php
+++ views/home.view.php
@@ -3,7 +3,7 @@ $title = "Home page";
 require 'layouts/header.layout.php';
 ?>
 <div class="container">
-    <h1>Home</h1>
+    <h1>Home page</h1>
     <p>This is site deployed with CI/CD on github actions</p>
     <p><a class="text-decoration-none" href="https://github.com/khamdullaevuz/cicd-test" target="_blank">Repo</a></p>
 </div>
